9d2c7f9226cf364ccbb18661bb83bb3424d67838
naveed ali abbasi <getinfo.naveed@gmail.com>
2025-03-26 00:48:52 +0500

    define use fatch

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -4,7 +4,7 @@ import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
 import axios from "axios";
-// import useFetch from "../../hooks/useFetch";
+import useFetch from "../../hooks/useFetch";
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
